9905391d91a0156c2879326d779839cece886db9
Charles Burgess <consulting@cvburgess.com>
2025-05-25 13:14:10 -0400

    deno fmt

--- src/scripts/toc.js
+++ src/scripts/toc.js
@@ -1,25 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   let currentActive = null;
 
-  const observer = new IntersectionObserver(entries => {
-    const visibleEntries = entries.filter(entry => entry.isIntersecting);
-    
+  const observer = new IntersectionObserver((entries) => {
+    const visibleEntries = entries.filter((entry) => entry.isIntersecting);
+
     // Set new active state to first visible entry
     if (visibleEntries.length > 0) {
-    
       // Clear previous active state
       if (currentActive) {
-        const activeLink = document.querySelector(`nav li a[href="#${currentActive}"]`);
+        const activeLink = document.querySelector(
+          `nav li a[href="#${currentActive}"]`,
+        );
         if (activeLink) {
-          activeLink.classList.remove('text-primary', 'font-medium');
+          activeLink.classList.remove("text-primary", "font-medium");
         }
       }
-    
+
       const firstVisible = visibleEntries[0];
-      const id = firstVisible.target.getAttribute('id');
+      const id = firstVisible.target.getAttribute("id");
       const link = document.querySelector(`nav li a[href="#${id}"]`);
       if (link) {
-        link.classList.add('text-primary', 'font-medium');
+        link.classList.add("text-primary", "font-medium");
         currentActive = id;
       }
     }
@@ -27,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Track all sections that have an `id` applied
   document
-    .querySelectorAll('h2[id], h3[id]')
+    .querySelectorAll("h2[id], h3[id]")
     .forEach((section) => {
       observer.observe(section);
     });
